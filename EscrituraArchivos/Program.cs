94a79a0ba9285f83ae397c00ae6a82cda47cefac
Sark1223 <105814864+Sark1223@users.noreply.github.com>
2022-06-01 18:28:01 -0700

    Tarea (prototipo)
    Sin:
    - excepciones
    - ni formatos

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -9,19 +9,68 @@ namespace EscrituraArchivos
 {
     class Program
     {
+        //creación de clase infoPersona
+        class infoPersona
+        {
+            public string nombre;
+            public sbyte edad;
+            public float peso;
+            public byte estatura;
+
+            //constructor de la clase 
+            public infoPersona(string nombre, sbyte edad, float peso, byte estatura)
+            {
+                this.nombre = nombre;
+                this.edad = edad;
+                this.peso = peso;
+                this.estatura = estatura;
+            }
+            
+            public DateTime fecha()
+            {
+                // Declaramos nuestra variable tipo fecha.
+                DateTime dtToday = DateTime.Now;
+                return dtToday;
+            }
+
+            public float MetodoIMC()
+            {
+                float estatura = this.estatura / 100;
+                return  this.peso / (estatura*estatura);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Escribiendo. . . \n");
-            
             //escritura en archivo
-            StreamWriter sw = new StreamWriter("escribir.txt", true);
-            
+            StreamWriter sw = new StreamWriter("Informacion.txt", true);
+            Console.WriteLine("Escribiendo. . . \n");
+
             //obtencion de datos 
-            Console.Write("Ingrese un nombre: ");
-            string name = Console.ReadLine();
+            Console.WriteLine("Favor de ingresar los siguientes datos.");
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Edad: ");
+            sbyte edad = sbyte.Parse(Console.ReadLine());
+
+            Console.Write("Peso (kg): ");
+            float peso = float.Parse(Console.ReadLine());
+
+            Console.Write("Estatura (cm) : ");
+            byte estatura = byte.Parse(Console.ReadLine());
+
+            //creacion de objeto tipo infoPersona
+            infoPersona Datos = new infoPersona(nombre, edad, peso, estatura);
 
             //escribirlo en el archivo "escribir"
-            sw.WriteLine(name);
+            sw.WriteLine("FECHA: {0}\n" +
+                         "NOMBRE: {1}\n" +
+                         "EDAD: {2} años\n" +
+                         "PESO: {3} kg\n" +
+                         "ESTATURA: {4}\n" +
+                         "IMC: {5}\n", 
+                         Datos.fecha(), Datos.nombre, Datos.edad, Datos.peso, Datos.estatura, Datos.MetodoIMC());
 
             //guardar cambios
             sw.Close();
