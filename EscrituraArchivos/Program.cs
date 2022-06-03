4a87624a8a0d9121fb9bab2f375faab65d97be7b
Sark1223 <105814864+Sark1223@users.noreply.github.com>
2022-06-02 23:15:53 -0700

    Tarea v1 (exepciones y ciclos)

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -35,48 +35,127 @@ namespace EscrituraArchivos
 
             public float MetodoIMC()
             {
-                float estatura = this.estatura / 100;
+                float estatura = this.estatura / 100F;
                 return  this.peso / (estatura*estatura);
             }
         }
 
         static void Main(string[] args)
         {
+            //Nombre del programa 
+            Console.Title = "REGISTRO DE DATOS";
+
             //escritura en archivo
-            StreamWriter sw = new StreamWriter("Informacion.txt", true);
-            Console.WriteLine("Escribiendo. . . \n");
+                StreamWriter sw = new StreamWriter("Informacion.txt", true);
+                Console.WriteLine("Escribiendo. . . \n");
+            
+            bool cerrar = false;
+            string decision;
+            do
+            {
+                //Declaracion de variables 
+                bool error = true;
+                string nombre;
+                sbyte edad;
+                float peso;
+                byte estatura;
 
-            //obtencion de datos 
-            Console.WriteLine("Favor de ingresar los siguientes datos.");
-            Console.Write("Nombre: ");
-            string nombre = Console.ReadLine();
+                do
+                {
+                    try
+                    {
+                        //obtencion de datos 
+                        Console.WriteLine("Favor de ingresar los siguientes datos.");
+                        Console.Write("Nombre: ");
+                        nombre = Console.ReadLine();
 
-            Console.Write("Edad: ");
-            sbyte edad = sbyte.Parse(Console.ReadLine());
+                        Console.Write("Edad: ");
+                        edad = sbyte.Parse(Console.ReadLine());
 
-            Console.Write("Peso (kg): ");
-            float peso = float.Parse(Console.ReadLine());
+                        Console.Write("Peso (kg): ");
+                        peso = float.Parse(Console.ReadLine());
 
-            Console.Write("Estatura (cm) : ");
-            byte estatura = byte.Parse(Console.ReadLine());
+                        Console.Write("Estatura (cm) : ");
+                        estatura = byte.Parse(Console.ReadLine());
 
-            //creacion de objeto tipo infoPersona
-            infoPersona Datos = new infoPersona(nombre, edad, peso, estatura);
+                        //creacion de objeto tipo infoPersona
+                        infoPersona Datos = new infoPersona(nombre, edad, peso, estatura);
 
-            //escribirlo en el archivo "escribir"
-            sw.WriteLine("FECHA: {0}\n" +
-                         "NOMBRE: {1}\n" +
-                         "EDAD: {2} años\n" +
-                         "PESO: {3} kg\n" +
-                         "ESTATURA: {4}\n" +
-                         "IMC: {5}\n", 
-                         Datos.fecha(), Datos.nombre, Datos.edad, Datos.peso, Datos.estatura, Datos.MetodoIMC());
+                        //escribirlo en el archivo "escribir"
+                        sw.WriteLine("FECHA: {0}\n" +
+                             "NOMBRE: {1}\n" +
+                             "EDAD: {2} años\n" +
+                             "PESO: {3} kg\n" +
+                             "ESTATURA: {4}\n" +
+                             "IMC: {5}\n",
+                             Datos.fecha(), Datos.nombre, Datos.edad, Datos.peso, Datos.estatura, Datos.MetodoIMC());
+                        
+                        Console.Clear();
+                        
+                        error = false;
+                    }
+                    //Revision de errores 
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Has ingresado muchos caracteres");
+                        Console.WriteLine("Presione enter para continuar. . . ");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Los datos no son del formato correcto");
+                        Console.WriteLine("Presione enter para continuar. . . ");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        Console.WriteLine("Presione enter para continuar. . . ");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
 
-            //guardar cambios
+                    
+
+                } while (error != false);
+
+                Console.WriteLine("Los datos ingresados son validos. . .\n");
+
+                //opcion para desidir si detener el programa o no.
+                do
+                {
+                    Console.Write("Desea registrar más datos?\n" +
+                                      "→ Si\n" +
+                                      "→ No\n" +
+                                      "Ingrese la opcion deseada: ");
+                    decision = Console.ReadLine();
+
+                    Console.Clear();
+
+                    //validacion de la respuesta que el ususario introdujo
+                    if (decision == "si" || decision == "Si" || decision == "sí" || decision == "Sí" || decision == "SÍ" || decision == "SI")
+                    {
+                        Console.WriteLine("Escribiendo. . . \n");
+                        cerrar = false;
+                    }
+                    else if (decision == "no" || decision == "NO" || decision == "No")
+                    {
+                        cerrar = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Favor de ingresar solo una de las 2 opciones dadas.");
+                        decision = "error";
+                    }
+
+                } while (decision == "error");
+
+            } while (cerrar == false);
+            Console.WriteLine("Se ha completado la escritura ☺\n");
+            Console.WriteLine("Presione enter para finalizar. . .");
             sw.Close();
-            Console.Clear();
-
-            Console.WriteLine("Se ha completado la escritura ☺");
             Console.ReadKey();
         }
     }
